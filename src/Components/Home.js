aa5069c7c36ee4597288910c4f7d1df344d36de7
Rodrigo Rojas <rorro.irg@gmail.com>
2019-08-03 10:47:49 -0400

    home page colums

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Home = () => {
     return(
-        <div>
-            <h1>This is home</h1>
+        <div className="container">
+            <h1>Welcome!</h1>
+            <div className="row">
+                <div className="col-md">
+                    <img src="" alt="Pic"></img>
+                </div>
+                <div className="col-md">
+                    <p>
+
+Ipsum mi purus luctus congue duis primis. Porta nullam aliquet pulvinar. Facilisi aliquet senectus commodo. Nostra potenti lacinia tempor dis sapien. Diam fusce quisque interdum. Nam cubilia dignissim torquent varius purus mollis luctus eget nascetur ut venenatis dictumst. Lectus cras phasellus molestie tortor. Magnis aptent lobortis; neque purus. Fusce nunc massa ipsum hac maecenas hendrerit nibh primis tincidunt maecenas ut. Congue sem venenatis diam elementum. Nascetur faucibus phasellus gravida proin diam. Suscipit faucibus mollis volutpat himenaeos. Maecenas nisl penatibus eget nulla rhoncus faucibus platea egestas tortor taciti.
+
+Ornare sit ultricies taciti pulvinar aenean sagittis. Eu hendrerit litora sollicitudin velit urna donec felis urna integer pretium? Mollis phasellus dis erat facilisi? Posuere etiam et consequat feugiat quis nunc egestas volutpat nascetur per. Augue libero vitae inceptos ridiculus mollis nunc lorem magnis malesuada class tincidunt nostra. Libero massa phasellus morbi ipsum orci.
+
+Hendrerit vehicula suspendisse auctor consectetur neque commodo auctor ridiculus natoque. Eget faucibus auctor bibendum hac posuere potenti magna sit proin dui nec inceptos. Parturient convallis, integer justo primis magnis. Per justo convallis primis vitae lacinia ultricies lectus habitasse quisque litora lobortis mi? Interdum laoreet sem eget sociosqu nunc eros lobortis vehicula quis in. Eleifend porttitor ligula felis lobortis imperdiet odio praesent pharetra elit. Torquent a tortor dis. Litora ullamcorper erat porta taciti dignissim aenean nibh fames platea nisl suscipit mus! Hendrerit ligula turpis vehicula ante in sociosqu facilisi amet dui metus.
+
+Nascetur maecenas purus tellus himenaeos sollicitudin curae; inceptos purus erat molestie congue dui! Velit hendrerit per curabitur pulvinar odio magnis sollicitudin. Eu consectetur urna est placerat sem dictum phasellus massa. Quam pellentesque magnis vulputate nisl diam quisque nulla. Cum purus curabitur magnis habitant urna. Consectetur litora amet mi nunc neque facilisi cursus ridiculus luctus risus! Habitant eros conubia fusce donec netus. Tristique primis posuere pulvinar quisque nulla gravida euismod aenean aliquet cum. Risus vehicula torquent fringilla elementum sociosqu fusce curabitur.
+
+Platea lacinia neque nisl nec sapien hac aliquam class. Lorem varius primis lacus accumsan iaculis. Aptent penatibus quis vivamus vulputate sagittis blandit. A accumsan, torquent morbi cursus facilisi eleifend. Phasellus sed sollicitudin cubilia ornare varius nullam luctus fringilla lacus sociis. Varius ullamcorper eleifend facilisi primis a habitant sociis dictumst nunc malesuada velit. Accumsan, tempus feugiat maecenas condimentum leo montes taciti! Netus sapien proin habitasse.
+
+</p>
+                </div>
+            </div>
         </div>
     );
     
