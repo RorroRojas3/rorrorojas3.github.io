b7eab1796684a357d9e43eef50d243b058614d9f
Rodrigo Rojas <rorro.irg@gmail.com>
2019-08-17 15:27:41 -0400

    Added EY logo

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -2,6 +2,7 @@ import React from "react";
 import HexawareLogo from "../Images/hexaware.jpg";
 import MagnaLogo from "../Images/magna.png";
 import FluorLogo from "../Images/fluor.jpg";
+import EYLogo from "../Images/ey.png";
 
 const Resume = () => {
   return (
@@ -25,10 +26,12 @@ const Resume = () => {
         alt="FluorLogo"
         style={logoStyle}
       />
+      <img className="image-fluid" src={EYLogo} alt="EYLogo" style={eyStyle} />
       <div className="d-none">
         https://hexaware.com/wp-content/themes/hexaware-main/images/Brand/downloadbrand/color/logo.jpg
         http://seekvectorlogo.com/wp-content/uploads/2018/03/magna-international-vector-logo.png
         https://newsroom.fluor.com/sites/fluor.newshq.businesswire.com/files/logo/image/Fluor_logo.jpg
+        https://worldvectorlogo.com/logo/ernst-young-ey
       </div>
     </div>
   );
@@ -37,6 +40,11 @@ const Resume = () => {
 const logoStyle = {
   width: "100px",
   height: "100xpx"
+};
+
+const eyStyle = {
+  width: "30px",
+  height: "30px"
 };
 
 const myContainer = {
