0d81c67d945dfbf36c9d3368664f0f9939002473
Rodrigo Rojas <rorro.irg@gmail.com>
2020-01-01 18:20:45 -0500

    Done with Hexaware

---
+++ src/Components/Company.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Company(props) {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-2 align-self-end">
+          <img
+            className="image-fluid"
+            width="100%"
+            height="100%"
+            src={props.logo}
+          ></img>
+        </div>
+        <div className="col-md-10 align-self-center">
+          <h4>{props.name}</h4>
+        </div>
+      </div>
+      <div className="row ml-5">
+        <ul>
+          {props.descriptions.map((description, key) => {
+            return <li>{description}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+const logoStyle = {
+  width: "100px",
+  height: "100px"
+};
+
+export default Company;
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,6 +3,7 @@ import React from "react";
 
 // My Components
 import Navbar from "./Navbar";
+import Company from "./Company";
 
 // Images
 import HexawareLogo from "../Images/hexaware.jpg";
@@ -10,49 +11,34 @@ import MagnaLogo from "../Images/magna.png";
 import FluorLogo from "../Images/fluor.jpg";
 import EYLogo from "../Images/ey.png";
 
+//
+const hexawareInfo = [
+  "Designed and implemented REST APIs by utilizing C# with .NET Core framework and Entity Framework Core.",
+  "Created and implemented database tables, views, and storage procedures into projects by utilizing SQL and Microsoft SQL Server.",
+  "Developed user interface of website by utilizing React.js library, HTML5, CSS3, and Bootstrap 4 framework.",
+  "Utilized Azure DevOps and Git for version control and revisions.",
+  "Developed projects in an agile, test-driven environment."
+];
+
 const Resume = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="container-fluid">
-        <img
-          className="image-fluid"
-          src={HexawareLogo}
-          alt="HexawareLogo"
-          style={logoStyle}
-        />
-        <img
-          className="image-fluid"
-          src={MagnaLogo}
-          alt="MagnaLogo"
-          style={logoStyle}
-        />
-        <img
-          className="image-fluid"
-          src={FluorLogo}
-          alt="FluorLogo"
-          style={logoStyle}
-        />
-        <img
-          className="image-fluid"
-          src={EYLogo}
-          alt="EYLogo"
-          style={eyStyle}
-        />
-        <div className="d-none">
+      <div className="container mt-4">
+        <Company
+          logo={HexawareLogo}
+          name="Hexaware Technologies"
+          descriptions={hexawareInfo}
+        ></Company>
+        {/* <div className="d-none">
           https://hexaware.com/wp-content/themes/hexaware-main/images/Brand/downloadbrand/color/logo.jpg
           http://seekvectorlogo.com/wp-content/uploads/2018/03/magna-international-vector-logo.png
           https://newsroom.fluor.com/sites/fluor.newshq.businesswire.com/files/logo/image/Fluor_logo.jpg
           https://worldvectorlogo.com/logo/ernst-young-ey
-        </div>
+        </div> */}
       </div>
     </div>
   );
-};
-
-const logoStyle = {
-  width: "100px",
-  height: "100xpx"
 };
 
 const eyStyle = {
