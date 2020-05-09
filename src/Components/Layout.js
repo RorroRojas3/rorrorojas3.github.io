9ee59afbe32919ede7816d745425c1ce3091549f
rorro <rorro@pop-os.localdomain>
2020-05-09 12:47:55 -0400

    Added layout

---
+++ src/Components/Layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Nav from "./Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Layout;
