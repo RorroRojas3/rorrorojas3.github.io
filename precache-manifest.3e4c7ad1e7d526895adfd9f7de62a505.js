004ff5322d09b0325c6ebb011eede87410f395f4
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-12-25 15:11:55 -0500

    Updates

---
+++ precache-manifest.3e4c7ad1e7d526895adfd9f7de62a505.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73b31aee726e565ab1fe961406c839c",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "68c53cb1b4677819792b",
+    "url": "/my-website/static/css/main.fcb9442d.chunk.css"
+  },
+  {
+    "revision": "c9c76d53251c7db68e7e",
+    "url": "/my-website/static/js/2.06a1bfee.chunk.js"
+  },
+  {
+    "revision": "1a1a42c150bd05496b97b265c552c789",
+    "url": "/my-website/static/js/2.06a1bfee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68c53cb1b4677819792b",
+    "url": "/my-website/static/js/main.1744d030.chunk.js"
+  },
+  {
+    "revision": "8a31e1a8153659a682c8",
+    "url": "/my-website/static/js/runtime-main.0cbcd75a.js"
+  },
+  {
+    "revision": "70f49e78be6fe6d7eb59294f5047146e",
+    "url": "/my-website/static/media/Rodrigo-Rojas-Resume.70f49e78.pdf"
+  },
+  {
+    "revision": "0aacd666556dc235bb8beb5cb1717b31",
+    "url": "/my-website/static/media/clemson.0aacd666.jpg"
+  },
+  {
+    "revision": "2f4e07a4f6db20ffae1aee8ef6b57d8c",
+    "url": "/my-website/static/media/fluor.2f4e07a4.jpg"
+  },
+  {
+    "revision": "ae31311e8ce24cb2d3d8d5733be5d455",
+    "url": "/my-website/static/media/github.ae31311e.png"
+  },
+  {
+    "revision": "60c95c22dc2a0cecdd55f76071582af5",
+    "url": "/my-website/static/media/hexaware.60c95c22.jpg"
+  },
+  {
+    "revision": "04bc0b7c1d041270d6ed1bcdcae59271",
+    "url": "/my-website/static/media/instagram.04bc0b7c.png"
+  },
+  {
+    "revision": "653826beb5e8feab330f18dfae27b126",
+    "url": "/my-website/static/media/profileContact.653826be.jpg"
+  }
+]);
